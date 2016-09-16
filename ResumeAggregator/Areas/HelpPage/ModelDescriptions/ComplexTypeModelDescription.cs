d70c2fc3409604f4b8139756765f04326ed18b4b
Nikitin-SV <Nikitin-SV@zsb.sbrf.ru>
2016-09-16 14:50:56 +0500

    Initialize commit

---
+++ ResumeAggregator/Areas/HelpPage/ModelDescriptions/ComplexTypeModelDescription.cs
@@ -0,0 +1,14 @@
+using System.Collections.ObjectModel;
+
+namespace ResumeAggregator.Areas.HelpPage.ModelDescriptions
+{
+    public class ComplexTypeModelDescription : ModelDescription
+    {
+        public ComplexTypeModelDescription()
+        {
+            Properties = new Collection<ParameterDescription>();
+        }
+
+        public Collection<ParameterDescription> Properties { get; private set; }
+    }
+}
